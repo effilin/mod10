8ec10ff0b2ba2ef4b683a906da20beac0f1c1e2b
effilin <effienummer@gmail.com>
2024-08-12 11:50:56 -0400

    app for beanstalk

--- lab1/index.js
+++ lab1/index.js
@@ -1,7 +1,6 @@
 import express from "express"; // import the express package
 import friendRoutes from './routes/friendRoutes.js';
-
-require('dotenv').config();
+import 'dotenv/config';
 
 const app = express(); // create a new app
 const port = 3000; // change this to run the app on a different port - usually a 4 digit number
