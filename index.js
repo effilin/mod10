d1d262bdb6736ed0d0432ef0a35334a271e2f7a6
effilin <effienummer@gmail.com>
2024-08-17 10:37:28 -0400

    upate to try pipeline

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ import friendRoutes from './routes/friendRoutes.js';
 import 'dotenv/config';
 
 
-const app = express(); // create a new app
-const port = 3030; // change this to run the app on a different port - usually a 4 digit number
+const app = express(); 
+const port = 3030; 
 
-// parse requests of content-type - application/json (needed for POST and PUT requests using req.body)
+
 app.use(express.json());
 
 app.use('/', express.static('public'))
